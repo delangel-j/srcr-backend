e5dcedfbba9f3496c6d43bf0a32fe3ccca26c44d
Jesus del Angel <delangelj711@gmail.com>
2021-07-21 23:49:34 -0500

    Proyecto de backend generado

---
+++ src/main/java/mca/srcr/SrcrBackendApplication.java
@@ -0,0 +1,13 @@
+package mca.srcr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrcrBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrcrBackendApplication.class, args);
+	}
+
+}
